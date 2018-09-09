d3b136393cadd37e4b1d3916c58d3b0bf399bc46
Antonio Moura <antonniogt@gmail.com>
2018-09-08 23:30:34 -0300

    Ajuste de layout reset password

--- src/containers/ResetPassword/index.js
+++ src/containers/ResetPassword/index.js
@@ -1,19 +1,8 @@
-import React, {Component} from 'react';
-import {
-  Button,
-  Card,
-  CardBody,
-  CardGroup,
-  Col,
-  Container,
-  Input,
-  InputGroup,
-  InputGroupAddon,
-  InputGroupText,
-  Row
-} from 'reactstrap';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Button, Card, CardBody, CardFooter, Col, Container, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+
 import {Form, Field} from 'react-final-form'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
@@ -24,76 +13,69 @@ const onSubmit = async values => {
 }
 
 
+
 class ResetPassword extends Component {
-
   render() {
-
-
     return (
-      <div className="app flex-row">
+      <div className="app flex-row align-items-center">
         <Container>
           <Row className="justify-content-center">
-            <Col md="8">
-              <CardGroup>
-                <Card className="p-1">
-                  <CardBody>
-                    <Form
-                      onSubmit={onSubmit}
-                      // initialValues={{stooge: 'larry', employed: false}}
-                      render={({handleSubmit, form, submitting, pristine, values}) => (
-                        <form onSubmit={handleSubmit}>
-                          <h1>Reset Password</h1>
-                          <strong>Nome de usuario:</strong>
-                          <InputGroup className="mb-4">
-                            <InputGroupAddon addonType="prepend">
-                              <InputGroupText>
-                                <i className="icon-lock"></i>
-                              </InputGroupText>
-                            </InputGroupAddon>
-                            <Field component="input" name="email">
-                              {({input, meta}) => (
-                                <div>
-                                  <Input {...input} type="text" placeholder="E-mail"/>
-                                </div>
-                              )}
-                            </Field>
+            <Col md="6">
+              <Card className="mx-4">
+                <CardBody className="p-4">
+                  <Form
+
+                    onSubmit={onSubmit}
+                    render={({handleSubmit, form, submitting, pristine, values}) => (
+                      <form onSubmit={handleSubmit}>
+
+                        <h1>Reset your password</h1>
+                        <p className="text-muted">Enter your email</p>
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="icon-user"></i>
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Field component="input" name="username">
+                            {({input, meta}) => (
+                              <Input {...input} type="text" placeholder="Username" autoComplete="username"component="input"/>
+                            )}
+                          </Field>
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>@</InputGroupText>
+                          </InputGroupAddon>
+
+                          <Field component="input" name="email">
+                            {({input, meta}) => (
+                              <Input {...input} type="email" placeholder="Email" autoComplete="email"component="input"/>
+                            )}
+                          </Field>
+
+                        </InputGroup>
+                        <Button color="success" block>Reset</Button>
 
 
-                          </InputGroup>
-
-                          <strong>Nome de usuario:</strong>
-                          <InputGroup className="mb-3">
-                            <InputGroupAddon addonType="prepend">
-                              <InputGroupText>@</InputGroupText>
-                            </InputGroupAddon>
-                            <Input type="text" placeholder="Email" autoComplete="email" name="email" component="input"/>
-                          </InputGroup>
-
-                          <div className="buttons">
-                            <button type="submit" disabled={submitting || pristine}>
-                              Submit
-                            </button>
-                            <button
-                              type="button"
-                              onClick={form.reset}
-                              disabled={submitting || pristine}
-                            >
-                              Reset
-                            </button>
-                          </div>
-                          <pre>{JSON.stringify(values, 0, 2)}</pre>
-                        </form>
-                      )}
-                    />
-
-
-                  </CardBody>
-                </Card>
-
-              </CardGroup>
+                        <pre>{JSON.stringify(values, 0, 2)}</pre>
+                      </form>
+                    )}
+                  />
+                </CardBody>
+                <CardFooter className="p-4">
+                  <Row>
+                    <Col xs="12" sm="6">
+                      <Button className="btn-facebook" block><span>facebook</span></Button>
+                    </Col>
+                    <Col xs="12" sm="6">
+                      <Button className="btn-twitter" block><span>twitter</span></Button>
+                    </Col>
+                  </Row>
+                </CardFooter>
+              </Card>
             </Col>
           </Row>
-
         </Container>
       </div>
     );
